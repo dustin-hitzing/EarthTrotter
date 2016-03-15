965614b1697f1e29cab427b41197dc75790ae3fe
Dustin Hitzing <dustinhitzing@Dustins-MacBook-Pro-3.local>
2016-03-14 20:17:09 -0400

    added number formatter

--- EarthTrotter/ConversionViewController.swift
+++ EarthTrotter/ConversionViewController.swift
@@ -10,13 +10,52 @@ import UIKit
 
 class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    //number formatter
     
-    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
-        if let text = textField.text where !text.isEmpty {
-            celsiusLabel.text = text
+    let numberFormatter: NSNumberFormatter = {
+        let nf = NSNumberFormatter()
+        nf.numberStyle = .DecimalStyle
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelciusLabel()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
+        }
+        else{
+            return nil
+        }
+    }
+    
+    func updateCelciusLabel(){
+        if let value = celsiusValue {
+            celsiusLabel.text = "\(value)"
         }
         else{
             celsiusLabel.text = "???"
         }
     }
+    
+    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+        if let text = textField.text, value = Double(text) {
+            fahrenheitValue = value
+        }
+        else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject){
+        textField.resignFirstResponder()
+    }
+    
 }
